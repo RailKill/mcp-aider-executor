ae5aeb80d5e84afceb93c4f053e5f5701d54c35e
RailKill <info@railkill.com>
2026-05-08 13:47:11 +0800

    Prepare build script

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { McpServer, StdioServerTransport } from "@modelcontextprotocol/server";
 import { cac } from "cac";
 import { registerMessageTool } from "./tools/message.js";
